a0941c9647d3fc95ea37846bda68afc6b5465509
josenyldo <josenildo.simao@gmail.com>
2021-01-15 10:32:17 -0300

    Uploading Project

---
+++ src/test/java/com/josenildo/applicationSpring/ApplicationSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josenildo.applicationSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
