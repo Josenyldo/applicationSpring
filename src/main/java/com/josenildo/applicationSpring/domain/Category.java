12c31ba5e2f5a9c65423410458b7016ddc5dcea8
josenyldo <josenildo.simao@gmail.com>
2021-02-15 19:14:50 -0300

    H2 database and automatic creation of the database.

--- src/main/java/com/josenildo/applicationSpring/domain/Category.java
+++ src/main/java/com/josenildo/applicationSpring/domain/Category.java
@@ -2,12 +2,18 @@ package com.josenildo.applicationSpring.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 //interface required for the object to be converted to a sequence of bytes
 public class Category implements Serializable {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private  Integer id;
 	private String nome;
 	
